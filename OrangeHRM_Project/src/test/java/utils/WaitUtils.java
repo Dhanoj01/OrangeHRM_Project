ec001e923582d91a8bd1488c1c039cf112b2250b
dhanojkumarsingh <dhanojkumar783@gmail.com>
2026-09-22 16:45:28 +0530

    config files

---
+++ OrangeHRM_Project/src/test/java/utils/WaitUtils.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitUtils {
+	
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	
+	public WaitUtils(WebDriver driver)
+	{
+		this.driver =  driver;
+		wait = new WebDriverWait(driver , Duration.ofSeconds(10));
+	}
+	
+	public void waitForElementVisible(WebElement element)
+	{
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+	
+	public void waitForElementClickable(WebElement element)
+	{
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+	}
+	
+	public void waitForUrlContains(String value)
+	{
+		wait.until(ExpectedConditions.urlContains(value));
+	}
+
+}
